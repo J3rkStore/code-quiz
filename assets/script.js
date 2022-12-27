bc920b6c7bb0a88eb6e3b8e6ae29231cf8df22e4
jerk-store <taran.leu@outlook.com>
2022-12-26 20:01:51 -0700

    adding reset css

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,29 @@
+var questionEl = document.querySelector(".question");
+var startButton = document.querySelector(".start-button");
+var questions = ["first question", "second question"];
 var timeLeft = 10;
+
+function loseGame() {
+  questionEl.textContent = "You lost";
+}
 
 function timer() {
   var timerInterval = setInterval(function () {
     timeLeft--;
     console.log(timeLeft);
-    if (timeLeft === 0) {
+    if (timeLeft < 1) {
+      loseGame();
       //stops interval
       clearInterval(timerInterval);
     }
   }, 100);
 }
 
-timer();
+function startGame() {
+  timeLeft = 10;
+  questionEl.textContent = questions[0];
+  timer();
+}
+
+startButton.addEventListener("click", startGame);
+//timer();
